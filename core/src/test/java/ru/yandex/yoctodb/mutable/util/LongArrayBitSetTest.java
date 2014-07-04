dc978ab18a64ed4a54900676977c1b46a7288fec
Vadim Tsesko <incubos@yandex.com>
2014-07-04 19:47:40 +0400

    Renamed LICENSE to COPYING and updated license header

--- core/src/test/java/ru/yandex/yoctodb/mutable/util/LongArrayBitSetTest.java
+++ core/src/test/java/ru/yandex/yoctodb/mutable/util/LongArrayBitSetTest.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright © 2014 Yandex
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See the COPYING file or
+ * http://www.wtfpl.net/ for more details.
+ */
+
 package ru.yandex.yoctodb.mutable.util;
 
 import com.google.common.primitives.Longs;
