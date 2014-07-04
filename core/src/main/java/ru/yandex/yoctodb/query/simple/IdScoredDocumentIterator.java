d9edbb74aa108cdd9a29e831d5581ef6acd36887
Vadim TSes'ko <incubos@users.noreply.github.com>
2014-07-04 19:47:40 +0400

    Renamed LICENSE to COPYING and updated license header

--- core/src/main/java/ru/yandex/yoctodb/query/simple/IdScoredDocumentIterator.java
+++ core/src/main/java/ru/yandex/yoctodb/query/simple/IdScoredDocumentIterator.java
@@ -1,5 +1,9 @@
 /*
- * Copyright (c) 2014 Yandex
+ * Copyright © 2014 Yandex
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See the COPYING file or
+ * http://www.wtfpl.net/ for more details.
  */
 
 package ru.yandex.yoctodb.query.simple;
