e09d2f1249262362df1b272ae156668278d47ca8
Vadim Tsesko <incubos@yandex-team.ru>
2015-01-20 14:02:30 +0300

    LICENSE minor fixes

--- core/src/main/java/com/yandex/yoctodb/util/immutable/ByteArrayIndexedList.java
+++ core/src/main/java/com/yandex/yoctodb/util/immutable/ByteArrayIndexedList.java
@@ -1,11 +1,11 @@
 /*
- * (C) YANDEX LLC, 2014
+ * (C) YANDEX LLC, 2014-2015
  *
  * The Source Code called "YoctoDB" available at
  * https://bitbucket.org/yandex/yoctodb is subject to the terms of the
- * Mozilla Public License, v. 2.0 (hereinafter - MPL).
+ * Mozilla Public License, v. 2.0 (hereinafter referred to as the "License").
  *
- * A copy of the MPL is available at http://mozilla.org/MPL/2.0/.
+ * A copy of the License is also available at http://mozilla.org/MPL/2.0/.
  */
 
 package com.yandex.yoctodb.util.immutable;
