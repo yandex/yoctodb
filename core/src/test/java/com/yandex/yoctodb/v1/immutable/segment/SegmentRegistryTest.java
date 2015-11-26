a6272cf49f9267fed927db8751c8b85388dae2de
Vadim Tsesko <incubos@yandex-team.ru>
2015-11-26 20:15:46 +0300

    m

--- core/src/test/java/com/yandex/yoctodb/v1/immutable/segment/SegmentRegistryTest.java
+++ core/src/test/java/com/yandex/yoctodb/v1/immutable/segment/SegmentRegistryTest.java
@@ -17,7 +17,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.util.NoSuchElementException;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit tests for {@link SegmentRegistry}
