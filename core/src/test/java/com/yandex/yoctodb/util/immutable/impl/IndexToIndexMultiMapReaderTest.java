7e9659f4a36e58ea9bf5ae6039a8a0aac92b0cf0
Vadim Tsesko <incubos@users.noreply.github.com>
2016-02-05 20:54:46 +0300

    Changed source code location in file headers

--- core/src/test/java/com/yandex/yoctodb/util/immutable/impl/IndexToIndexMultiMapReaderTest.java
+++ core/src/test/java/com/yandex/yoctodb/util/immutable/impl/IndexToIndexMultiMapReaderTest.java
@@ -2,7 +2,7 @@
  * (C) YANDEX LLC, 2014-2015
  *
  * The Source Code called "YoctoDB" available at
- * https://bitbucket.org/yandex/yoctodb is subject to the terms of the
+ * https://github.com/yandex/yoctodb is subject to the terms of the
  * Mozilla Public License, v. 2.0 (hereinafter referred to as the "License").
  *
  * A copy of the License is also available at http://mozilla.org/MPL/2.0/.
