dc37f165d182ec57f01c5d1d1f75a39ba3155aae
irenkamalova <irenkamalova@yandex-team.ru>
2019-02-14 12:07:38 +0300

    added tests

--- core/src/test/java/com/yandex/yoctodb/util/mutable/stored/ManyNonUniqueValuesTest.java
+++ core/src/test/java/com/yandex/yoctodb/util/mutable/stored/ManyNonUniqueValuesTest.java
@@ -14,44 +14,49 @@ import java.io.IOException;
 import static com.yandex.yoctodb.mutable.DocumentBuilder.IndexOption.STORED;
 import static org.junit.Assert.assertEquals;
 
-public class ManyUniqueValuesStoredIndex {
+public class ManyNonUniqueValuesTest {
     private String fieldName = "test";
     @Test
-    public void values100() throws IOException {
+    public void oneByte() throws IOException {
         testDB(100);
     }
-
     @Test
-    public void values300() throws IOException {
+    public void twoByte() throws IOException {
         testDB(300);
         /*
         Result:
-        Write 300 values in 41 ms
-        Read database in 12 ms
-        Read 300 values in 5 ms
+        Write 290 values in 10 ms
+        Read database in 76 ms
+        Read 10 values in 0 ms
+        Read 290 in 3 ms
          */
     }
 
     @Test
-    public void values70000() throws IOException {
+    public void fourByte() throws IOException {
         testDB(70000);
         /*
-        Result
-        Write 70000 values in 321 ms
-        Read database in 12 ms
-        Read 70000 values56 ms
-         */
+        Result:
+        Write 69990 values in 265 ms
+        Read database in 53 ms
+        Read and compare 10 values in 0 ms
+        Read and compare 69990 in 56 ms
+        */
     }
 
     private void testDB(final int size) throws IOException {
         final DatabaseBuilder dbBuilder =
                 DatabaseFormat.getCurrent().newDatabaseBuilder();
+        // init first 10 elements by the same values
+        for (int i = 0; i < 10; i++) {
+            dbBuilder.merge(buildTestDocument(Integer.toString((0))));
+        }
         long start = System.currentTimeMillis();
-        for (int i = 0; i < size; i++) {
+        for (int i = 10; i < size; i++) {
             dbBuilder.merge(buildTestDocument(Integer.toString((i))));
         }
         long end = System.currentTimeMillis();
-        System.out.println("Write " + size +
+        System.out.println("Write " + (size - 10) +
                 " values in " +
                 (end - start) + " ms");
         final ByteArrayOutputStream os = new ByteArrayOutputStream();
@@ -65,18 +70,26 @@ public class ManyUniqueValuesStoredIndex {
         end = System.currentTimeMillis();
         System.out.println("Read database in " + (end - start) + " ms");
 
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 10; i++) {
+            assertEquals(Integer.toString((0)),
+                    UnsignedByteArrays.toString(
+                            UnsignedByteArrays.from(db.getFieldValue(i, fieldName))));
+        }
+        end = System.currentTimeMillis();
 
+        System.out.println("Read and compare 10 values in " +
+                (end - start) + " ms");
 
         start = System.currentTimeMillis();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 10; i < 100; i++) {
             assertEquals(Integer.toString((i)),
                     UnsignedByteArrays.toString(
                             UnsignedByteArrays.from(db.getFieldValue(i, fieldName))));
         }
         end = System.currentTimeMillis();
-        System.out.println("Read " + size + " values in " +
+        System.out.println("Read and compare " + (size - 10) + " in " +
                 (end - start) + " ms");
-
     }
 
     private DocumentBuilder buildTestDocument(String value) {
@@ -84,4 +97,3 @@ public class ManyUniqueValuesStoredIndex {
                 .withField(fieldName, value, STORED);
     }
 }
-
